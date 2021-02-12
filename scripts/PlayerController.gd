4fd303360ac43bd5964d446dfad858e2f1f8f398
LightStorm12345 <lightmeow12345@gmail.com>
2021-02-11 23:53:33 -0600

    player jumping work in progress

--- scripts/PlayerController.gd
+++ scripts/PlayerController.gd
@@ -1,20 +1,23 @@
 extends KinematicBody
 
 var speed = 500 # Player Speed
+var jumpingForce = 1200
 
 var speedH = 0.1 # Camera movement speed
 var speedV = 0.1 # Camera movement speed
 
-var gravity = 900 # gravity speed
+var gravity = 475 # gravity speed, def 900
 var noClip = true # Is player in noclip mode?
 var outOfBoundsLimit = -25 # Limit before the player teleported back to the map
 
 var yaw = 0.0
 var pitch = 0.0
 
+var allowJumping
 	
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	allowJumping = true
 	
 func _input(event):
 	if Input.is_key_pressed(KEY_N):
@@ -34,6 +37,21 @@ func _input(event):
 func _physics_process(delta):
 	# noclip stuff
 	$CollisionShape.disabled = noClip
+	
+	# Checking if player is allowed to jump
+	var space_state = get_world().direct_space_state
+	var cast_from = self.transform.origin + Vector3(0, 3, 0)
+	var cast_to = Vector3(self.transform.origin.x, -100, self.transform.origin.z)
+	var result = space_state.intersect_ray(cast_from, cast_to, [self])
+	
+	# if the player is above 2.92 then they cant jump
+	#allowJumping = (self.transform.origin.y - result.position.y) <= 2.92
+	#print(allowJumping)
+	
+	# debug stuff, just shows some info on the hud
+	get_node("../UI/Noti").text = "CURRENT POSITION: " + String(self.transform.origin)
+	
+	
 	
 	# make sure that the player didn't fall of the map
 	#if transform.origin.y < outOfBoundsLimit:
@@ -77,6 +95,12 @@ func _physics_process(delta):
 	if Input.is_key_pressed(KEY_D):
 		# warning-ignore:return_value_discarded
 		move_and_slide(sideways * -1 * speed * delta)
+		
+	# jumping
+	if Input.is_key_pressed(KEY_SPACE) and allowJumping == true:
+		allowJumping = false
+		# warning-ignore:return_value_discarded
+		move_and_slide(Vector3.UP * jumpingForce * delta)
 		
 	# Camera movement
 	if pitch >= 90:
--- scripts/TerrainGeneratorNEW.gd
+++ scripts/TerrainGeneratorNEW.gd
@@ -1,4 +1,4 @@
-# THIS IS NOT MY SCRIPT, I ONLY MODIFIED IT
+# THIS IS NOT MY SCRIPT, I ONLY MODIFIED IT AND COMMENTED IT
 # ALL CREDIT FOR THE SCRIPT GOES TO CODAT (YOUTUBE)
 # https://www.youtube.com/channel/UCSWBKqm0Rxxi-yZa0zyvDjA
 
@@ -10,37 +10,46 @@ func _ready():
 	noise.period = 200
 	noise.octaves = 6
 	
-	var plane_mesh = PlaneMesh.new()
-	plane_mesh.size = Vector2(400, 400)
-	plane_mesh.subdivide_depth = 200
-	plane_mesh.subdivide_width = 200
+	# PRESETS FOR TERRAIN:
+	# p = 200 & o = 8 : Slightly Hilly Terrain
 	
-	var surface_tool = SurfaceTool.new()
-	surface_tool.create_from(plane_mesh, 0)
 	
-	var array_plane = surface_tool.commit()
 	
-	var data_tool = MeshDataTool.new()
+	var plane_mesh = PlaneMesh.new() # create the plane
+	plane_mesh.size = Vector2(400, 400) # set the size of the plane
+	plane_mesh.subdivide_depth = 200 # subdivide the plane in half
+	plane_mesh.subdivide_width = 200 # subdivice the plane in half on another axis
 	
-	data_tool.create_from_surface(array_plane, 0)
+	var surface_tool = SurfaceTool.new() # create surface tool
+	surface_tool.create_from(plane_mesh, 0) # create surface tool "mesh" from the plane
 	
-	for i in range(data_tool.get_vertex_count()):
-		var vertex = data_tool.get_vertex(i)
-		vertex.y = noise.get_noise_3d(vertex.x, vertex.y, vertex.z) * 60
+	var array_plane = surface_tool.commit() # create the plane array from surface tool
+	
+	var data_tool = MeshDataTool.new() # create MeshDataTool
+	
+	data_tool.create_from_surface(array_plane, 0) # create the "mesh" from array_plane
+	
+	for i in range(data_tool.get_vertex_count()): # loop through each vertex
+		var vertex = data_tool.get_vertex(i) # get the current vertex we are working with
+		vertex.y = noise.get_noise_3d(vertex.x, vertex.y, vertex.z) * 60 # scale the y value by the noise value
 		
-		data_tool.set_vertex(i, vertex)
+		data_tool.set_vertex(i, vertex) # set the vertex to our modified value
 		
-	for i in range(array_plane.get_surface_count()):
-		array_plane.surface_remove(i)
+	# NOTE:
+	# not sure what his happening from here on out, still gonna try to comment it though
 	
-	data_tool.commit_to_surface(array_plane)
-	surface_tool.begin(Mesh.PRIMITIVE_TRIANGLES)
-	surface_tool.create_from(array_plane, 0)
-	surface_tool.generate_normals()
+	for i in range(array_plane.get_surface_count()): # loop through each surface in array_plane
+		array_plane.surface_remove(i) # remove the surface
 	
-	var mesh_instance = MeshInstance.new()
-	mesh_instance.mesh = surface_tool.commit()
-	mesh_instance.create_trimesh_collision()
-	mesh_instance.set_surface_material(0, load("res://terrain.material"))
+	data_tool.commit_to_surface(array_plane) # commit to surface using the array_plane
+	surface_tool.begin(Mesh.PRIMITIVE_TRIANGLES) # make a new mesh using the triangles primitive
+	surface_tool.create_from(array_plane, 0) # create the mesh from array_plane
+	surface_tool.generate_normals() # generate the normals
 	
-	add_child(mesh_instance)
+	# I know what is happening here
+	var mesh_instance = MeshInstance.new() # create a new MeshInstance
+	mesh_instance.mesh = surface_tool.commit() # set the mesh from surface tool
+	mesh_instance.create_trimesh_collision() # setup the collion using the mesh
+	mesh_instance.set_surface_material(0, load("res://terrain.material")) # apply the terrain material
+	
+	add_child(mesh_instance) # add it as a child
