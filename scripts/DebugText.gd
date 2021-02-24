86d84c78694313222eb8092afc13f6c471be6bb0
LightStorm12345 <lightmeow12345@gmail.com>
2021-02-24 09:47:36 -0600

    working on main menu

---
+++ scripts/DebugText.gd
@@ -0,0 +1,9 @@
+extends Label
+
+#var player
+#
+#func _ready():
+#	 player = get_node("../../Player")
+#
+#func _process(delta):
+#	text = "Player Position: " + String(player.transform.origin)
---
+++ scripts/MainMenuController.gd
@@ -0,0 +1,22 @@
+extends Control
+
+var newGamePanel
+var settingsPanel
+
+func _ready():
+	newGamePanel = get_node("Panels/NewGamePanel")
+	settingsPanel = get_node("Panels/SettingsPanel")
+	
+# New Game
+func _on_NewGame_button_down():
+	settingsPanel.visible = false
+	newGamePanel.visible = not newGamePanel.visible
+
+# Settings
+func _on_Settings_button_down():
+	newGamePanel.visible = false
+	settingsPanel.visible = not settingsPanel.visible
+	
+# Quit Game
+func _on_Quit_button_down():
+	get_tree().quit()
