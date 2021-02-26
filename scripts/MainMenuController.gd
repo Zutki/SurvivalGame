f2c7db7604644f2c64c0e78bf9aad80ac1d704a5
LightStorm12345 <lightmeow12345@gmail.com>
2021-02-26 01:42:08 -0600

    Updated Main Menu New World Creation Panel

--- scripts/MainMenuController.gd
+++ scripts/MainMenuController.gd
@@ -7,6 +7,7 @@ func _ready():
 	newGamePanel = get_node("Panels/NewGamePanel")
 	settingsPanel = get_node("Panels/SettingsPanel")
 	
+	
 # New Game
 func _on_NewGame_button_down():
 	settingsPanel.visible = false
@@ -20,3 +21,4 @@ func _on_Settings_button_down():
 # Quit Game
 func _on_Quit_button_down():
 	get_tree().quit()
+
